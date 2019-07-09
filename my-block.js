cb5346ffa7c663c845b307aa7fd16270aba936bb
Johanne <johanne_lewis@hotmail.com>
2019-07-09 17:57:03 +0100

    Save content that is entered in input field

--- my-block.js
+++ my-block.js
@@ -7,8 +7,12 @@ wp.blocks.registerBlockType('johanne/border-box', {
 		color: {type: 'string'}
 	},
 	edit: function(props){
-		return wp.element.createElement("div", null, wp.element.createElement("h3", null, "Your Cool Border Box"), 
-			wp.element.createElement("input", {type: "text"}));
+		function updateContent(event){
+			props.setAttributes({content: event.target.value})
+		}
+		return React.createElement("div", null, React.createElement("h3", null, "Your Cool Border Box"), 
+			React.createElement("input", 
+				{type: "text",value: props.attributes.content, onChange: updateContent}));
 	},
 	save: function(props){
 		return null
