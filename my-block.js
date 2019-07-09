a607364a1ffb03a1ed66a332489f0818fc7cb646
Johanne <johanne_lewis@hotmail.com>
2019-07-09 17:47:47 +0100

    Create an input field for editing block

--- my-block.js
+++ my-block.js
@@ -1,1 +1,16 @@
-alert("hello block editor");
+wp.blocks.registerBlockType('johanne/border-box', {
+	title: 'My Cool Border Box',
+	icon: 'smiley',
+	category: 'common',
+	attributes: {
+		content: {type: 'string'},
+		color: {type: 'string'}
+	},
+	edit: function(props){
+		return wp.element.createElement("div", null, wp.element.createElement("h3", null, "Your Cool Border Box"), 
+			wp.element.createElement("input", {type: "text"}));
+	},
+	save: function(props){
+		return null
+	} 
+})
