a8e7db17b2cb63fe7a4e7c4e39a3db7de1c50875
Johanne <johanne_lewis@hotmail.com>
2019-07-09 18:05:02 +0100

    Add a color picker and method to save color

--- my-block.js
+++ my-block.js
@@ -10,9 +10,14 @@ wp.blocks.registerBlockType('johanne/border-box', {
 		function updateContent(event){
 			props.setAttributes({content: event.target.value})
 		}
+		function updateColor(value){
+			props.setAttributes({color: value.hex})
+		}
 		return React.createElement("div", null, React.createElement("h3", null, "Your Cool Border Box"), 
 			React.createElement("input", 
-				{type: "text",value: props.attributes.content, onChange: updateContent}));
+				{type: "text", value: props.attributes.content, onChange: updateContent}), 
+			React.createElement(wp.components.ColorPicker, 
+				{color: props.attributes.color, onChangeComplete: updateColor}));
 	},
 	save: function(props){
 		return null
